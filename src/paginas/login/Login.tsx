c6a9d8548be9cc7767a116aa400f9a87d6982323
LucasCapSilva <57732522+LucasCapSilva@users.noreply.github.com>
2021-12-17 09:12:12 -0300

    Update Login.tsx

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -69,12 +69,12 @@ function Login() {
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrarrr</Typography>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
                         <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
                         <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
                                 <Button type='submit' variant='contained' color='primary'>
-                                    Logarr
+                                    Logar
                                 </Button>
                         </Box>
                     </form>
